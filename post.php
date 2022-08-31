3f6c7f5681f2713130e805be0eb888ea07d51232
NIRVXSH <setthanan2544@gmail.com>
2022-08-31 10:53:54 +0700

    add php

---
+++ post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align: center;">NIRVXSH</h1><hr>
+    <center>
+        ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"] ?><br>
+        <table style="border: 2px solid black; width: 40%;">
+        <tr><td align="center" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
+        <tr><td align="center"><textarea name="comment" id="" cols="80%" rows="10%"></textarea></td></tr>
+        <tr><td align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+        </table>
+        <a href="index.html">กลับสู่หน้าหลัก</a>
+    </center>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 style="text-align: center;">NIRVXSH</h1><hr>
+    <center>
+        เข้าสู่ระบบด้วย<br>
+        <?php 
+            $login=$_POST['login'];
+            $passwordlogin=$_POST['passwordlogin'];
+            echo "Login = $login <BR>";
+            echo "Password = $passwordlogin <BR>";
+        ?>
+    </center>
+</body>
+</html>
+
+
+
+
